683040bc51244887de2f3598d70d440a7df7e3fd
Stephan Nielsen <stephannielsen@gmail.com>
2018-05-04 20:50:57 +0200

    Layout modifications (exercise2).

--- and101/TipCalculator/TipCalculator/Resources/Resource.Designer.cs
+++ and101/TipCalculator/TipCalculator/Resources/Resource.Designer.cs
@@ -45,7 +45,25 @@ namespace TipCalculator
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int editText1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int linearLayout2 = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView2 = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView3 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView4 = 2131034118;
 			
 			static Id()
 			{
