5a2be8c78ea9e08ed1fb4b928063a71e4a9e9a63
Stephan Nielsen <stephannielsen@gmail.com>
2018-05-04 22:39:25 +0200

    Added link to xamarin.com in about page

--- and102/GroceryList/AboutActivity.cs
+++ and102/GroceryList/AboutActivity.cs
@@ -6,20 +6,23 @@ using Android.Widget;
 
 namespace GroceryList
 {
-	[Activity(Label = "About")]			
-	public class AboutActivity : Activity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			SetContentView(Resource.Layout.About);
+  [Activity(Label = "About")]
+  public class AboutActivity : Activity
+  {
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+      SetContentView(Resource.Layout.About);
 
-			FindViewById<Button>(Resource.Id.learnMoreButton).Click += OnLearnMoreClick;
-		}
+      FindViewById<Button>(Resource.Id.learnMoreButton).Click += OnLearnMoreClick;
+    }
 
-		void OnLearnMoreClick(object sender, EventArgs e)
-		{
-			// TODO
-		}
-	}
+    void OnLearnMoreClick(object sender, EventArgs e)
+    {
+      var intent = new Intent(Intent.ActionView);
+      intent.SetData(Android.Net.Uri.Parse("http://www.xamarin.com"));
+      if (intent.ResolveActivity(PackageManager) != null)
+        StartActivity(intent);
+    }
+  }
 }
