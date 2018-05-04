16f816300c3a9bd36bf5cd30cefe37b570bd8336
Stephan Nielsen <stephannielsen@gmail.com>
2018-05-04 22:08:17 +0200

    Added navigation to item detail (exercise4)

--- and102/GroceryList/DetailsActivity.cs
+++ and102/GroceryList/DetailsActivity.cs
@@ -4,22 +4,20 @@ using Android.Widget;
 
 namespace GroceryList
 {
-	[Activity(Label = "Details")]			
-	public class DetailsActivity : Activity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			SetContentView(Resource.Layout.Details);
+  [Activity(Label = "Details")]
+  public class DetailsActivity : Activity
+  {
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+      SetContentView(Resource.Layout.Details);
 
-			int position = 0;
+      int position = Intent.Extras.GetInt("ItemPosition", -1);
 
-			// TODO
+      var item = MainActivity.Items[position];
 
-			var item = MainActivity.Items[position];
-
-			FindViewById<TextView>(Resource.Id.nameTextView ).Text = "Name: "  + item.Name;
-			FindViewById<TextView>(Resource.Id.countTextView).Text = "Count: " + item.Count.ToString();
-		}
-	}
+      FindViewById<TextView>(Resource.Id.nameTextView).Text = "Name: " + item.Name;
+      FindViewById<TextView>(Resource.Id.countTextView).Text = "Count: " + item.Count.ToString();
+    }
+  }
 }
--- and102/GroceryList/ItemsActivity.cs
+++ and102/GroceryList/ItemsActivity.cs
@@ -7,24 +7,26 @@ using Android.Widget;
 
 namespace GroceryList
 {
-	[Activity(Label = "Items")]			
-	public class ItemsActivity : Activity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			SetContentView(Resource.Layout.Items);
+  [Activity(Label = "Items")]
+  public class ItemsActivity : Activity
+  {
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+      SetContentView(Resource.Layout.Items);
 
-			var lv = FindViewById<ListView>(Resource.Id.listView);
-			lv.Adapter = new ArrayAdapter<Item>(this, Android.Resource.Layout.SimpleListItem1, Android.Resource.Id.Text1, MainActivity.Items);	
-			lv.ItemClick += OnItemClick;
-		}
+      var lv = FindViewById<ListView>(Resource.Id.listView);
+      lv.Adapter = new ArrayAdapter<Item>(this, Android.Resource.Layout.SimpleListItem1, Android.Resource.Id.Text1, MainActivity.Items);
+      lv.ItemClick += OnItemClick;
+    }
 
-		void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
-		{
-			int position = e.Position; // e.Position is the position in the list of the item the user touched
+    void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+    {
+      int position = e.Position; // e.Position is the position in the list of the item the user touched
 
-			// TODO
-		}
-	}
+      var intent = new Intent(this, typeof(DetailsActivity));
+      intent.PutExtra("ItemPosition", position);
+      StartActivity(intent);
+    }
+  }
 }
