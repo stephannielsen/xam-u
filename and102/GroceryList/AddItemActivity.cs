59b10a8e0aead225c7ca24358b61a9b19b15e9d6
Stephan Nielsen <stephannielsen@gmail.com>
2018-05-04 22:00:42 +0200

    Added cancel functionality for AddItemActivity.

--- and102/GroceryList/AddItemActivity.cs
+++ and102/GroceryList/AddItemActivity.cs
@@ -6,29 +6,29 @@ using Android.Widget;
 
 namespace GroceryList
 {
-	[Activity(Label = "Add Item")]			
-	public class AddItemActivity : Activity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			SetContentView(Resource.Layout.AddItem);
+  [Activity(Label = "Add Item")]
+  public class AddItemActivity : Activity
+  {
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+      SetContentView(Resource.Layout.AddItem);
 
-			FindViewById<Button>(Resource.Id.saveButton  ).Click += OnSaveClick;
-			FindViewById<Button>(Resource.Id.cancelButton).Click += OnCancelClick;
-		}
+      FindViewById<Button>(Resource.Id.saveButton).Click += OnSaveClick;
+      FindViewById<Button>(Resource.Id.cancelButton).Click += OnCancelClick;
+    }
 
-		void OnSaveClick(object sender, EventArgs e)
-		{
-			string name  = FindViewById<EditText>(Resource.Id.nameInput).Text;
-			int    count = int.Parse(FindViewById<EditText>(Resource.Id.countInput).Text);
+    void OnSaveClick(object sender, EventArgs e)
+    {
+      string name = FindViewById<EditText>(Resource.Id.nameInput).Text;
+      int count = int.Parse(FindViewById<EditText>(Resource.Id.countInput).Text);
 
-			// TODO
-		}
+      // TODO
+    }
 
-		void OnCancelClick(object sender, EventArgs e)
-		{
-			// TODO
-		}
-	}
+    void OnCancelClick(object sender, EventArgs e)
+    {
+      Finish();
+    }
+  }
 }
